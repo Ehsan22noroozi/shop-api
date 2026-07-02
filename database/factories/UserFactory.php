a20564a34b3db6ca1f4f34f92488b9a77e7b06bc
Ehsan <ehsan.22.noroozi@gmail.com>
2026-07-02 17:14:36 +0330

    Update ecommerce database structure

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,20 +26,20 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'mobile' => fake()->unique()->numerify('09#########'),
+            'mobile_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model's mobile number should be unverified.
      */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'mobile_verified_at' => null,
         ]);
     }
 }
